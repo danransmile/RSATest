cc0ba90151ad473d7e730a3d0d12dcd7dc2b90dc
刘艳芹 <liuyanqin@imdada.cn>
2019-09-09 13:19:59 +0800

    上传RSA代码

---
+++ RSA/JSRSA.h
@@ -0,0 +1,28 @@
+//
+//  JSRSA.h
+//  JSEncryptModule
+//
+//  Created by JetHuang on 07/06/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSRSA : NSObject
+// return base64 encoded string
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
+// return raw data
++ (NSData *)encryptData:(NSData *)data publicKey:(NSString *)pubKey;
+// return base64 encoded string
+// enc with private key NOT working YET!
+//+ (NSString *)encryptString:(NSString *)str privateKey:(NSString *)privKey;
+// return raw data
+//+ (NSData *)encryptData:(NSData *)data privateKey:(NSString *)privKey;
+
+// decrypt base64 encoded string, convert result to string(not base64 encoded)
++ (NSString *)decryptString:(NSString *)str publicKey:(NSString *)pubKey;
++ (NSData *)decryptData:(NSData *)data publicKey:(NSString *)pubKey;
++ (NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;
++ (NSData *)decryptData:(NSData *)data privateKey:(NSString *)privKey;
+
+
+@end
